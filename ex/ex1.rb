8d8dd5736e7124c1f98a5eb82e5b0e8abe340bf0
Rob Sobers <rsobers@gmail.com>
2011-10-10 22:07:38 -0400

    Add executable exercise files. Fix some typos.

--- ex/ex1.rb
+++ ex/ex1.rb
@@ -1,1 +1,7 @@
-# Some code here.
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts 'Yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
