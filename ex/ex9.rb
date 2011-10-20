0925b5a7731a17684606816f10ecf74099927c86
Zed A. Shaw <zedshaw@zedshaw.com>
2011-10-20 12:32:15 -0700

    Error in the text from the Python version.

--- ex/ex9.rb
+++ ex/ex9.rb
@@ -8,7 +8,7 @@ puts "Here are the months: ", months
 
 puts <<PARAGRAPH
 There's something going on here.
-With the three double-quotes.
+With the PARAGRAPH thing
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
 PARAGRAPH
